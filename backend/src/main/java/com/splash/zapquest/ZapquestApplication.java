b02ab56180fe8b02d4894c81cc45552db95b1e56
Weiming Zheng <weiming.zheng.ca@gmail.com>
2024-10-21 10:47:34 -0400

    Add backend and frontend subproject to the main project
    Initialize the backend with Spring Boot Initializer.

---
+++ backend/src/main/java/com/splash/zapquest/ZapquestApplication.java
@@ -0,0 +1,13 @@
+package com.splash.zapquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZapquestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZapquestApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/splash/zapquest/ZapquestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.splash.zapquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZapquestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
