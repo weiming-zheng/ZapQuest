d2887805a4cf6bf9715b83bccfad70e22ec2d9c6
Weiming Zheng <weiming.zheng.ca@gmail.com>
2024-11-30 16:15:26 -0500

    Quick fix the bug "is_deleted" for database column

--- backend/src/main/java/com/splash/zapquest/pojo/entity/BaseEntity.java
+++ backend/src/main/java/com/splash/zapquest/pojo/entity/BaseEntity.java
@@ -1,10 +1,7 @@
 package com.splash.zapquest.pojo.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -29,6 +26,7 @@ public abstract class BaseEntity {
     private LocalDateTime updatedAt;
 
     @Column(nullable = false)
+    @Builder.Default
     private Boolean isDeleted = false;
 
 
