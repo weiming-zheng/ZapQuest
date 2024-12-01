802983d04ebc177db2e531427a98859543cdd6d6
leo19980000 <neil826864579@gmail.com>
2024-12-01 11:46:19 -0500

    reward module is working with backend

--- frontend/src/pages/rewards/RedeemList.js
+++ frontend/src/pages/rewards/RedeemList.js
@@ -2,16 +2,20 @@ import React from "react";
 import "./RedeemList.css";
 import coin from "../../assets/coin.png"
 
-function RedeemList({title,price,isRedeemed,onRedeem }){
+function RedeemList({id,iconId,title,price,isRedeemed,onRedeem }){
     return(
         <div className="redeemed">
-            <p className = "avatar">🍦</p>
+            <p className = "avatar">{iconId}</p>
             <h4 className="title1">{title}</h4>
             <p className="price">{"Price: "+price}</p>
             <img  className = "coin" src = {coin} />
             <div className="post-actions">
-                <button className={`btn ${isRedeemed ? "redeemed-btn" : "redeem-btn"}`} onClick={onRedeem} disabled={isRedeemed} >
-                  {isRedeemed ? 'Redeemed' : 'Redeem'} 
+            <button 
+                className={`btn ${isRedeemed ? "redeemed-btn" : "redeem-btn"}`} 
+                onClick={onRedeem}  // 直接调用onRedeem，因为已经在父组件绑定了参数
+                disabled={isRedeemed}
+            >
+                    {isRedeemed ? 'Redeemed' : 'Redeem'}
                 </button>
             </div>
         </div>
