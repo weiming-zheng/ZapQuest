ffe407f68172807faaf3d572facab82016b7d999
Weiming Zheng <weiming.zheng.ca@gmail.com>
2024-12-01 15:07:50 -0500

    Add CORS configuration

---
+++ backend/src/main/java/com/splash/zapquest/server/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.splash.zapquest.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/v1/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600); // 1 hour cache for preflight requests
+    }
+}
